bf673f46603f497c5b79add85522cd947b5f6bbf
adamprime <adamprime@hey.com>
2025-04-08 13:09:16 -0500

    Update requests to 2.32.0 to fix security vulnerability

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         "feedgen>=0.9.0",
         "flask>=2.0.0",
-        "requests>=2.25.0",
+        "requests>=2.32.0",
         "beautifulsoup4>=4.9.0",
         "pytz>=2021.1",
         "selenium>=4.0.0",
